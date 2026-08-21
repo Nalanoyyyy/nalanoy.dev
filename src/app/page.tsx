4c1b40136ba7d9bf855fefc6db739c66b93e4c1c
Nalanoyyyy <booberbboobibi@gmail.com>
2026-08-21 15:03:27 +0700

    fix: avatar size on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,7 @@ export default function Home() {
             </div>
           </div>
           <div className="avatar">
-            <Image src="/profile.png" alt={name} width={350} height={350} style={{ objectFit: "cover" , objectPosition: "center 30%" }} />
+            <Image src="/profile.png" alt={name} width={400} height={400} style={{ objectFit: "cover" , objectPosition: "center top" }} />
           </div>
         </div>
       </header>
